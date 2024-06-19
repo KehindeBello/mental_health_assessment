7f0c0e6740842cad7f647b2d004c28af58bd8cd9
kehindebello <adedotunomomeji@gmail.com>
2024-06-19 10:08:45 +0100

    questions and response mappings

---
+++ questions_mappings.py
@@ -0,0 +1,39 @@
+questions = {
+    "phq9_1": "Little interest or pleasure in doing things",
+    "phq9_2": "Feeling down, depressed, or hopeless",
+    "phq9_3": "Trouble falling or staying asleep, or sleeping too much",
+    "phq9_4": "Feeling tired or having little energy",
+    "phq9_5": "Poor appetite or overeating",
+    "phq9_6": "Feeling bad about yourself or that you are a failure or have let yourself or your family down",
+    "phq9_7": "Trouble concentrating on things, such as reading the newspaper or watching television",
+    "phq9_8": "Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual",
+    "phq9_9": "Thoughts that you would be better off dead or hurting yourself in some way",
+    "gad7_1": "Feeling nervous, anxious, or on edge",
+    "gad7_2": "Not being able to stop or control worrying",
+    "gad7_3": "Worrying too much about different things",
+    "gad7_4": "Trouble relaxing",
+    "gad7_5": "Being so restless that is hard to sit still",
+    "gad7_6": "Becoming easily annoyed or irritable",
+    "gad7_7": "Feeling afraid as if something awful might happen",
+    "pss_1": "In the last month, how often have you been upset because of something that happened unexpectedly?",
+    "pss_2": "In the last month, how often have you felt confident about your ability to handle problems?",
+    "pss_3": "In the last month, how often have you felt that things were going your way?",
+    "pss_4": "In the last month, how often have you felt difficulties were piling up so high that you could not overcome them?"
+    } 
+
+phq9_mapping = {
+        "0": "Not at all",
+        "1": "Several days",
+        "2": "More than half the days",
+        "3": "Nearly every day"
+    }
+
+gad7_mapping = phq9_mapping  # Same mapping for GAD-7
+
+pss4_mapping = {
+        "0": "Never",
+        "1": "Almost never",
+        "2": "Sometimes",
+        "3": "Fairly often",
+        "4": "Very often"
+}
